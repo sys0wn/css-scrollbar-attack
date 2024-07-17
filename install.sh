ed8e1c75825cfe94e90412c172507f0978e935b8
sys0wn <138518220+sys0wn@users.noreply.github.com>
2024-07-17 17:31:44 +0200

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,4 +1,4 @@
-apt install apache fontforge npm -y
+apt install apache2 fontforge npm -y
 cd shop.appCssInjectionDemo
 npm install
 head -n -2 /etc/apache2/sites-enabled/000-default-le-ssl.conf >> temp && mv temp /etc/apache2/sites-enabled/000-default-le-ssl.conf
