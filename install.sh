dca879ce55d10140182033d7d685f17e31fa7a4a
sys0wn <138518220+sys0wn@users.noreply.github.com>
2024-07-18 23:06:30 +0200

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,8 +1,8 @@
 apt install apache2 fontforge npm -y
 cd shop.appCssInjectionDemo
 npm install
-head -n -2 /etc/apache2/sites-enabled/000-default-le-ssl.conf >> temp && mv temp /etc/apache2/sites-enabled/000-default-le-ssl.conf
-printf "ProxyPass / http://localhost:3000/\nProxyPassReverse / http://localhost:3000/\n\n</VirtualHost>\n</IfModule>" > /etc/apache2/sites-enabled/000-default-le-ssl.conf
+sed -i -e '$ d' /etc/apache2/sites-enabled/000-default-le-ssl.conf && sed -i -e '$ d' /etc/apache2/sites-enabled/000-default-le-ssl.conf
+printf "ProxyPass / http://localhost:3000/\nProxyPassReverse / http://localhost:3000/\n\n</VirtualHost>\n</IfModule>" >> /etc/apache2/sites-enabled/000-default-le-ssl.conf
 a2enmod proxy proxy_http
 systemctl restart apache2
 node index.js
