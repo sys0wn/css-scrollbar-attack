d2043ad7e465610d920b186368ff1a788cb67bb5
sys0wn <138518220+sys0wn@users.noreply.github.com>
2024-07-26 16:44:28 +0200

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,12 +1,12 @@
 apt install apache2 fontforge npm certbot python3-certbot-apache -y
-cd css-scrollbar-attack
 npm install
 sed -i -e '$ d' /etc/apache2/sites-enabled/000-default-le-ssl.conf && sed -i -e '$ d' /etc/apache2/sites-enabled/000-default-le-ssl.conf
 printf "ProxyPass / http://localhost:3000/\nProxyPassReverse / http://localhost:3000/\n\n</VirtualHost>\n</IfModule>" >> /etc/apache2/sites-enabled/000-default-le-ssl.conf
 a2enmod proxy proxy_http
 systemctl restart apache2
-echo "Please enter the domain name that is currently linked to this VPS (e.g example.com)"
+printf "\n\nPlease enter the domain name that is currently linked to this VPS (e.g example.com):"
 read domainName
 sed -i "s/exfil.server/$domainName/g" index.html index.js
 printf "Y\n$domainName\n/var/www/html\n" | certbot --authenticator webroot --installer apache --register-unsafely-without-email
+clear
 node index.js
