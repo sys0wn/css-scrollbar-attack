cbb8537e5c4ee8b20ee8afc3f65b985778b8e93a
sys0wn <138518220+sys0wn@users.noreply.github.com>
2024-07-26 16:39:12 +0200

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,4 +1,4 @@
-apt install apache2 fontforge npm -y
+apt install apache2 fontforge npm certbot python3-certbot-apache -y
 cd css-scrollbar-attack
 npm install
 sed -i -e '$ d' /etc/apache2/sites-enabled/000-default-le-ssl.conf && sed -i -e '$ d' /etc/apache2/sites-enabled/000-default-le-ssl.conf
@@ -8,4 +8,5 @@ systemctl restart apache2
 echo "Please enter the domain name that is currently linked to this VPS (e.g example.com)"
 read domainName
 sed -i "s/exfil.server/$domainName/g" index.html index.js
+printf "Y\n$domainName\n/var/www/html\n" | certbot --authenticator webroot --installer apache --register-unsafely-without-email
 node index.js
